1850985e6a6a4cb3dd43bc68bf994da4e2911d95
bendanPLUS <32793865+DuYuNeu@users.noreply.github.com>
2019-10-02 00:05:02 +0800

    创建文件的pom

---
+++ src/main/java/com/algorithm/bendanplus/BendanplusApplication.java
@@ -0,0 +1,9 @@
+package com.algorithm.bendanplus;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class BendanplusApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BendanplusApplication.class, args);
+	}
+}
