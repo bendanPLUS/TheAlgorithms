de1a79837c323bd07ccef36142fea89b646f3458
bendanPLUS <32793865+DuYuNeu@users.noreply.github.com>
2019-10-01 23:40:45 +0800

    Initial commit

---
+++ src/test/java/com/algorithm/bendanplus/TestOne.java
@@ -0,0 +1,13 @@
+package com.algorithm.bendanplus;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class TestOne {
+	@Test
+	public void testCheckActivityApi() {
+		System.out.println("--------");
+	}
+}
